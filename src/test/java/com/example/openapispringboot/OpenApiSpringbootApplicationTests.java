f55fc18b3550c328ec0d959afe89c47a9b3bd3c0
Eric Gitonga Njue <eric.gitonga@imbank.co.ke>
2020-10-01 13:00:26 +0300

    Initial Project Creation

---
+++ src/test/java/com/example/openapispringboot/OpenApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
